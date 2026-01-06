1b60814ad83c3f14fb8aadc0dcf553d86dcdd0fa
ikioideok <said99mcx@gmail.com>
2026-01-06 14:42:59 +0900

    Set router basename for subdirectory deploy

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import AdminChat from './AdminChat.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/tokutei" element={<Tokutei />} />
